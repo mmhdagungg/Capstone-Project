1859a63a4e22cbbf0706fffb9c8e2d5bfa32564f
RafliKamal <rafli.gamdroin@gmail.com>
2024-12-04 16:07:58 +0700

    fragment insert dasar

--- Cashify/app/src/main/java/com/kamalapp/cashify/MainActivity.kt
+++ Cashify/app/src/main/java/com/kamalapp/cashify/MainActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.kamalapp.cashify.databinding.ActivityMainBinding
 
@@ -20,16 +18,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
+        supportActionBar?.hide()
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_insert, R.id.navigation_history
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+
+
         navView.setupWithNavController(navController)
     }
 }
