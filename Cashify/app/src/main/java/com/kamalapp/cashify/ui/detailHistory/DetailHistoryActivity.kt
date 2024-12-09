f7da2b7e9425727f3a7a66aebf774a20a1994800
RafliKamal <rafli.gamdroin@gmail.com>
2024-12-09 10:54:37 +0700

    ubah layout login

--- Cashify/app/src/main/java/com/kamalapp/cashify/ui/detailHistory/DetailHistoryActivity.kt
+++ Cashify/app/src/main/java/com/kamalapp/cashify/ui/detailHistory/DetailHistoryActivity.kt
@@ -1,10 +1,7 @@
 package com.kamalapp.cashify.ui.detailHistory
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.kamalapp.cashify.R
 
 class DetailHistoryActivity : AppCompatActivity() {
