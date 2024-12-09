ff4bf587942e3e07d9fc8276eed3a9bd120bd5cc
RafliKamal <rafli.gamdroin@gmail.com>
2024-12-09 19:08:24 +0700

    welcome masih belum final

--- Cashify/app/src/main/java/com/kamalapp/cashify/ui/welcome/WelcomeActivity.kt
+++ Cashify/app/src/main/java/com/kamalapp/cashify/ui/welcome/WelcomeActivity.kt
@@ -1,21 +1,28 @@
 package com.kamalapp.cashify.ui.welcome
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.kamalapp.cashify.R
+import com.kamalapp.cashify.ui.login.LoginActivity
+import com.kamalapp.cashify.ui.register.RegisterActivity
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_welcome)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+        val btnMulai = findViewById<Button>(R.id.btnMulai)
+
+        btnMulai.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
         }
+
+
     }
 }
