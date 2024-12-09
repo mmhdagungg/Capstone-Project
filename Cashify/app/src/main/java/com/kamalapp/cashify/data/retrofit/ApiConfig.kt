d0b63310e7f5670c8a6edbef40d3450484331f81
RafliKamal <rafli.gamdroin@gmail.com>
2024-12-09 19:24:12 +0700

    register udah bisa

---
+++ Cashify/app/src/main/java/com/kamalapp/cashify/data/retrofit/ApiConfig.kt
@@ -0,0 +1,17 @@
+package com.kamalapp.cashify.data.retrofit
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiConfig {
+
+    private const val BASE_URL = "https://cashify-api-586910673966.asia-southeast1.run.app"
+
+    val instance: ApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiService::class.java)
+    }
+}
