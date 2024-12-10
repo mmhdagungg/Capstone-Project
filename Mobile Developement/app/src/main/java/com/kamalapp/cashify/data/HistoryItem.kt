272fcfeab7529e57937e192dfd11e63f78cc875c
RafliKamal <rafli.gamdroin@gmail.com>
2024-12-10 21:47:24 +0700

    coba lagi

--- Mobile Developement/app/src/main/java/com/kamalapp/cashify/data/HistoryItem.kt
+++
@@ -1,7 +0,0 @@
-package com.kamalapp.cashify.data
-
-data class HistoryItem(
-    val title: String,
-    val date: String,
-    val iconRes: Int
-)
--- Mobile Developement/app/src/main/java/com/kamalapp/cashify/ui/insert/InsertViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.kamalapp.cashify.ui.insert
-
-import androidx.lifecycle.ViewModel
-
-class InsertViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
